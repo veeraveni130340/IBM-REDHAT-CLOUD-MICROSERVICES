57e519fce512a8d52bf29c4f1f0ecfb1c1b85150
veeraveni130340 <85543571+veeraveni130340@users.noreply.github.com>
2021-07-12 18:15:12 +0530

    Add files via upload

---
+++ hello-world-service/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "hello-world";
+		
+	}
+
+}
